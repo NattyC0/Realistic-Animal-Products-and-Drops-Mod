aea2fd8182ed996b5d1843d814dccbbc710f1372
RoyalKingZB <zsenshinobaki@gmail.com>
2017-06-14 21:31:18 -0700

    Hi

---
+++ src/main/java/net/sargeant/rapad/main/RAPADMod.java
@@ -0,0 +1,26 @@
+package net.sargeant.rapad.main;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Joshua on 6/14/2017.
+ */
+public class RAPADMod
+{
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+}
