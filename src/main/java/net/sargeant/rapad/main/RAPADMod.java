b550021ab75dfccd4aafc9974c5e6550cf8e1504
RoyalKingZB <zsenshinobaki@gmail.com>
2017-06-14 21:43:44 -0700

    Hi

--- src/main/java/net/sargeant/rapad/main/RAPADMod.java
+++ src/main/java/net/sargeant/rapad/main/RAPADMod.java
@@ -1,5 +1,7 @@
 package net.sargeant.rapad.main;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -7,8 +9,16 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 /**
  * Created by Joshua on 6/14/2017.
  */
+@Mod(modid = RAPADMod.MODID, version = RAPADMod.VERSION, name = RAPADMod.NAME)
 public class RAPADMod
 {
+    public static final String MODID = "rapad";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "Realistic Animal Products and Drops Mod";
+
+    @SidedProxy(clientSide = "net.sargeant.rapad.proxy.ClientProxy", serverSide = "net.sargeant.rapad.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
     public void preInit(FMLPreInitializationEvent event)
     {
 
@@ -21,6 +31,6 @@ public class RAPADMod
 
     public void postInit(FMLPostInitializationEvent event)
     {
-        
+
     }
 }
---
+++ src/main/java/net/sargeant/rapad/proxy/ClientProxy.java
@@ -0,0 +1,9 @@
+package net.sargeant.rapad.proxy;
+
+/**
+ * Created by Joshua on 6/14/2017.
+ */
+public class ClientProxy extends CommonProxy
+{
+
+}
